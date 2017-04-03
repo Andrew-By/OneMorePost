72ffe4dfc3f0b88804b22aec43b93f24879f0a28
Андрей Бычков <andrew-by@hotmail.com>
2017-04-03 22:10:13 +0300

    Изменения в структуре по результатам согласования

---
+++ src/OneMorePost/Models/VKAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneMorePost.Models
+{
+    public class VKAccount
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
