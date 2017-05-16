15ef25e815708cdc075a1d51be61c31161b5e7be
Иван Еленский <elenskivan@hotmail.com>
2017-05-16 22:23:02 +0300

    Dirty fully functional commit, close #3, #6

--- src/OneMorePost/Models/Author.cs
+++ src/OneMorePost/Models/Author.cs
@@ -11,6 +11,7 @@ namespace OneMorePost.Models
     public class Author
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
     }
 }
